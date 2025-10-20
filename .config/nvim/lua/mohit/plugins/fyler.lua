44e4cfbbe90a5109f3b4d1770e7a2f724ea509c4
SinghMohit <nishadamar989@gmail.com>
2025-10-20 19:10:36 +0530

    add fyler and modify harpoon keybindings

---
+++ .config/nvim/lua/mohit/plugins/fyler.lua
@@ -0,0 +1,85 @@
+return {
+    "A7Lavinraj/fyler.nvim",
+    dependencies = {"nvim-mini/mini.icons"},
+    branch = "stable",
+    opts = {icon_provider = "nvim_web_devicons"},
+    config = function()
+        local fyler = require("fyler")
+        fyler.setup(
+            {
+                -- Close explorer when file is selected
+                close_on_select = true,
+                -- Auto-confirm simple file operations
+                confirm_simple = false,
+                -- Replace netrw as default explorer
+                default_explorer = false,
+                -- Git integration
+                git_status = {
+                    enabled = true,
+                    symbols = {
+                        Untracked = "?",
+                        Added = "+",
+                        Modified = "*",
+                        Deleted = "x",
+                        Renamed = ">",
+                        Copied = "~",
+                        Conflict = "!",
+                        Ignored = "#"
+                    }
+                },
+                hooks = {
+                    -- function(path) end
+                    on_delete = nil,
+                    -- function(src_path, dst_path) end
+                    on_rename = nil,
+                    -- function(hl_groups, palette) end
+                    on_highlight = nil
+                },
+                -- Directory icons
+                icon = {
+                    directory_collapsed = nil,
+                    directory_empty = nil,
+                    directory_expanded = nil
+                },
+                -- Icon provider (none, mini_icons or nvim_web_devicons)
+                icon_provider = "mini_icons",
+                -- Indentation guides
+                indentscope = {
+                    enabled = true,
+                    group = "FylerIndentMarker",
+                    marker = "│"
+                },
+                -- Key mappings
+                mappings = {
+                    ["q"] = "CloseView",
+                    ["<CR>"] = "Select",
+                    ["<C-t>"] = "SelectTab",
+                    ["|"] = "SelectVSplit",
+                    ["-"] = "SelectSplit",
+                    ["^"] = "GotoParent",
+                    ["="] = "GotoCwd",
+                    ["."] = "GotoNode",
+                    ["#"] = "CollapseAll",
+                    ["<BS>"] = "CollapseNode"
+                },
+                popups = {
+                    permission = {}
+                },
+                -- Buffer tracking
+                track_current_buffer = true,
+                -- Window configuration
+                win = {
+                    -- Window border style
+                    border = "rounded",
+                    -- Default window kind
+                    kind = "replace",
+                    -- Window kind presets
+                    kind_presets = {},
+                    -- Buffer and window options
+                    buf_opts = {}, -- Custom buffer options
+                    win_opts = {} -- Custom window options
+                }
+            }
+        )
+    end
+}
--- .config/nvim/lua/mohit/plugins/harpoon.lua
+++ .config/nvim/lua/mohit/plugins/harpoon.lua
@@ -1,13 +1,52 @@
 return {
     "ThePrimeagen/harpoon",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
+    branch = "harpoon2",
+    lazy = false,
+    dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
-      local keymap = vim.keymap
-      keymap.set("n", "<leader>ha", "<cmd>lua require('harpoon.mark').add_file()<CR>", { desc = "adds a file in harpoon" })
-      keymap.set("n", "<leader>h", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>", { desc = "toggles the harpoon menu" })
-      keymap.set("n", "<leader>hn", "<cmd>lua require('harpoon.ui').nav_next()<CR>", { desc = "Go to next file in harpoon" })
-      keymap.set("n", "<leader>hp", "<cmd>lua require('harpoon.ui').nav_prev()<CR>", { desc = "Go to previous file in harpoon" })
+        local harpoon = require("harpoon")
+
+        harpoon:setup()
+
+        vim.keymap.set("n", "<leader>a", function()
+            harpoon:list():add()
+        end)
+        vim.keymap.set("n", "<leader>h", function()
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+        end)
+
+        -- Switch to harpooned file 1-4
+        vim.keymap.set("n", "<leader>1", function()
+            harpoon:list():select(1)
+        end)
+        vim.keymap.set("n", "<leader>2", function()
+            harpoon:list():select(2)
+        end)
+        vim.keymap.set("n", "<leader>3", function()
+            harpoon:list():select(3)
+        end)
+        vim.keymap.set("n", "<leader>4", function()
+            harpoon:list():select(4)
+        end)
+
+        -- susbstitute(replace the current file with the harpoons
+        -- with this the given index ) 
+        -- harpoon file 1-4 with new files
+        vim.keymap.set("n", "<leader><C-q>", function()
+            harpoon:list():replace_at(1)
+        end)
+        vim.keymap.set("n", "<leader><C-w>", function()
+            harpoon:list():replace_at(2)
+        end)
+        vim.keymap.set("n", "<leader><C-e>", function()
+            harpoon:list():replace_at(3)
+        end)
+        vim.keymap.set("n", "<leader><C-r>", function()
+            harpoon:list():replace_at(4)
+        end)
+
+        -- Toggle previous & next buffers stored within Harpoon list
+        -- vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+        -- vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
     end,
 }
